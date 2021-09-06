cb1dc4d25f1c06dc526908950ef4e31f336f81b4
Calle <calle.leppajoki@gmail.com>
2021-09-06 21:08:54 +0200

    angular init

---
+++ traggla/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SessionComponent } from './session/session.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SessionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
